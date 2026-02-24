0be837e4f44b0a9c5dd5961d8c7e60aa56c409a9
Nouresme <nour-ines.riahi@esme.fr>
2026-02-24 20:37:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
-import tkinter 
-
-# creating the grid 
-def draw_grid():
-  for col in range(3):
-    for ro in range(3):
-      button = tkinter.Button(
-        root, font=("Arial", 100), 
-        text="X", 
-        width=5 height=3
-      )
-      button.grid(row=ro, column=col) 
+import tkinter as tk
 
 #creating a window
-root = tkinter.Tk()
+root = tk.Tk()
+
+
+
+#when the current player clicks, its symbol gets put on the grid 
+def place_symbol(row, column):
+
+    clicked_button = buttons[column][row]
+    if clicked_button['text'] == "":
+        clicked_button.config(text=current_player)
+
+
+
+#drawing the grid
+def draw_grid():
+    for column in range(3):
+        buttons_in_cols = []
+        for row in range(3):
+            button = tk.Button(
+                root, font=("Arial", 50),
+                width=5, height=2,
+                command=lambda r=row, c=column: place_symbol(r, c)
+            )
+            button.grid(row=row, column=column)
+            buttons_in_cols.append(button)
+        buttons.append(buttons_in_cols)
 
 
 #personnalising the window
 root.title("tictactoe")
-root.minsize(600 , 600) 
+root.minsize(600 , 600)
+
+draw_grid()
 root.mainloop 
+
