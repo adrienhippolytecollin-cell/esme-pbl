a27aeae81aa0d1c97515cd4cee3bfd0a2582254b
adrienhippolytecollin-cell <adrien.hippolyte.collin@gmail.com>
2026-03-02 16:07:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import tkinter as tk
+from tkinter import messagebox
 
 #creating the window
 root = tk.Tk()
 
+#creating the status
+status_label = tk.Label(root, text="Current Player: X", font=("Arial", 15))
+status_label.grid(row=3, column=0, columnspan=3)
+
+#game mode selection
+game_mode_var = tk.StringVar(value="HUMAN")
+difficulty_var = tk.StringVar(value="EASY")
 
 # stocking base variables 
 buttons = []
@@ -15,6 +23,10 @@ def print_winner():
     if win is False:
         win = True
         print("Player", current_player, "has won! Congratulations!")
+        show_popup_winner()
+
+def show_popup_winner():
+    messagebox.showinfo("Game Over", f"Player {current_player} wins!")
 
 
 #finding winning combinations
@@ -79,12 +91,18 @@ def switch_player():
 #putting the corresponding symbol when a player clicks somewhere on the grid if the space is empty, then after each play we check if there is a win and if not we change player 
 def place_symbol(row, column):
 
+    global win
+    if win:
+        return
+
     clicked_button = buttons[column][row]
     if clicked_button['text'] == "":
         clicked_button.config(text=current_player)
 
         check_win(row, column)
         switch_player()
+        if game_mode_var.get() == "AI" and current_player == "0" and not win:
+            ai_move(difficulty_var.get())
 
 
 # drawing the grid inside a function to have a more efficient code (it endend up working) 
@@ -101,12 +119,34 @@ def draw_grid():
             buttons_in_cols.append(button)
         buttons.append(buttons_in_cols)
 
+#the restart button 
+def restart_game():
+    global win, current_player
+    win = False
+    current_player = "X"
+    status_label.config(text="Current Player: X")
 
+    for col in range(3):
+        for row in range(3):
+            buttons[col][row].config(text="")
+
+#empty
+def ai_move(difficulty):
+    print("AI plays in", difficulty, "mode")
 
 # parameters of the window
 root.title("New game of TicTacToe")
 root.minsize(500, 500)
+restart_button = tk.Button(root, text="Restart", font=("Arial", 15), command=restart_game)
+restart_button.grid(row=4, column=0, columnspan=3)
+
+tk.Radiobutton(root, text="Human vs Human", variable=game_mode_var, value="HUMAN").grid(row=5, column=0)
+tk.Radiobutton(root, text="Human vs AI", variable=game_mode_var, value="AI").grid(row=5, column=2)
+
+difficulty_menu = tk.OptionMenu(root, difficulty_var, "EASY", "MEDIUM", "HARD")
+difficulty_menu.grid(row=6, column=0, columnspan=3)
 
 draw_grid()
 root.mainloop()
+
 
